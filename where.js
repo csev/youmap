755a6c3a023bcbfc74e176d7cbac0d3fee71f9ef
Charles Severance <csev@umich.edu>
2017-10-27 13:23:40 -0400

    Add new location

--- where.js
+++ where.js
@@ -1,4 +1,5 @@
 myData = [
+[37.79177,-122.40275, 'Coursera Office Hours San Francisco 19-Oct-2017','dQ1YZMU1DHg'],
 [39.94849,-75.14372, 'Coursera Office Hours Philadelphia 07-June-2017','O5ESxp-IPEs'],
 [49.28313,-123.11214, 'Coursera Office Hours Vancouver, BC Canada 25-May-2017','l3iDVYEJwyQ'],
 [47.61524,-122.34112, 'Coursera Office Hours Seattle,WA 23-May-2017','7ScIlzEcKMg'],
